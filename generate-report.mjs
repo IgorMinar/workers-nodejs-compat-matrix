0f823bbc9ec6678f8000da43d9fd839bd554842e
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-03 14:43:49 -0500

    Refactor generate-report to work on other computers

--- generate-report.mjs
+++ generate-report.mjs
@@ -1,28 +1,47 @@
-import shell from 'shelljs';
+import shell from "shelljs";
 
-shell.set('-e');
+shell.set("-e");
 
-const volta=  '/Users/iminar/.volta/bin/volta';
+if (!shell.env.VOLTA_HOME) {
+  console.error(
+    "You must have volta installed to continue. Refer to README.md for instructions."
+  );
+  process.exit(1);
+}
 
-shell.echo('Generate node v18 apis...');
-shell.exec(volta + ' run --node 18 node node/dump.mjs');
-shell.echo('=== Done ====================================\n\n');
+// shelljs doesn't read from .bashrc or .zshrc which normally inject VOLTA_HOME
+// into your PATH variable, so a lookup is needed.
+// Trailing space is intentional, for DX
+const volta = `${shell.env.VOLTA_HOME}/bin/volta `;
 
-shell.echo('Generate node v20 apis...');
-shell.exec(volta + ' run --node 20 node node/dump.mjs');
-shell.echo('=== Done ====================================\n\n');
+// Node
+const nodeVersions = [18, 20, 22];
+for (const version of nodeVersions) {
+  shell.echo(`Generate node v${version} apis...`);
+  shell.exec(volta + `run --node ${version} node node/dump.mjs`);
+  shell.echo("=== Done ====================================\n\n");
+}
 
-shell.echo('Generate node v22 apis...');
-shell.exec(volta + ' run --node 22 node node/dump.mjs');
-shell.echo('=== Done ====================================\n\n');
+shell.echo("Make node v22 apis the baseline...");
+shell.cp("node/apis-22.json", "node/apis.json");
+shell.echo("=== Done ====================================\n\n");
 
-shell.echo('Make node v22 apis the baseline...');
-shell.cp('node/apis-22.json', 'node/apis.json');
-shell.echo('=== Done ====================================\n\n');
+// Workerd
+shell.echo("Generate workerd + --node_compat apis...");
+shell.exec("node workerd/dump.mjs");
+shell.echo("=== Done ====================================\n\n");
 
-shell.echo('Generate workerd + --node_compat apis...');
-shell.exec('node workerd/dump.mjs');
-shell.echo('=== Done ====================================\n\n');
+// Wrangler with polyfills
+shell.echo("Generate wrangler-v3 + --node_compat apis...");
+shell.exec(volta + "run --node 20 node wrangler-v3-polyfills/dump.mjs");
+shell.echo("=== Done ====================================\n\n");
 
-// TODO: add all three wrangler versions
+shell.echo("Generate wrangler-jspm + --node_compat apis...");
+shell.exec(volta + "run --node 20 node wrangler-jspm-polyfills/dump.mjs");
+shell.echo("=== Done ====================================\n\n");
+
+shell.echo("Generate wrangler-unenv + --node_compat apis...");
+shell.exec(volta + "run --node 20 node wrangler-unenv-polyfills/dump.mjs");
+shell.echo("=== Done ====================================\n\n");
+
 // TODO: invoke script to generate the report
