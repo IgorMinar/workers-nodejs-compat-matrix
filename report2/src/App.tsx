e19724689286f379918262d9e4ad258b01786536
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-05 16:19:02 -0500

    Support multiple targets

--- report2/src/App.tsx
+++ report2/src/App.tsx
@@ -55,6 +55,15 @@ const isObject = (nodeValue: any) =>
   typeof nodeValue === "object" &&
   Object.keys(nodeValue).length > 0;
 
+const targetTitles = {
+  node18: "Node 18",
+  node20: "Node 20",
+  node22: "Node 22",
+  workerd: "workerd",
+  wranglerJspm: "wrangler (w/ jspm)",
+  wranglerUnenv: "wrangler (w/ unenv)",
+  wranglerV3: "wrangler (v3)",
+};
 function App() {
   const getTargetValue: any = (map: CompatMap, path: string[]) => {
     if (path.length === 0) {
@@ -66,27 +75,54 @@ function App() {
     return getTargetValue(value as CompatMap, tail);
   };
 
-  const target = node18 as any as CompatMap;
-
-  const renderEntries: any = (baseline: any, path: string[]) => {
+  const renderEntries: any = (
+    baseline: any,
+    targets: Record<string, CompatMap>,
+    path: string[]
+  ) => {
     const rows = [];
     let baselineTotal = 0;
-    let targetTotal = 0;
+
+    let targetTotals: Record<string, number> = Object.keys(targets).reduce(
+      (acc, key) => ({ ...acc, [key]: 0 }),
+      {}
+    );
 
     for (const [key, nodeValue] of Object.entries(baseline)) {
       const keyPath = [...path, key];
-      const targetSupported = getTargetValue(target, keyPath);
 
       let childRows;
+
       if (isObject(nodeValue)) {
-        const result = renderEntries(nodeValue, keyPath);
-        debugger;
+        const result = renderEntries(nodeValue, targets, keyPath);
         childRows = result.rows;
         baselineTotal += result.baselineTotal;
-        targetTotal += result.targetTotal;
+        for (const targetKey of Object.keys(targets)) {
+          targetTotals[targetKey] += result.targetTotals[targetKey];
+        }
       } else {
         baselineTotal += 1;
-        targetTotal += targetSupported ? 1 : 0;
+        for (const targetKey of Object.keys(targets)) {
+          const targetSupported = getTargetValue(targets[targetKey], keyPath);
+          targetTotals[targetKey] += targetSupported ? 1 : 0;
+        }
+      }
+
+      let columns = [];
+      for (const targetKey of Object.keys(targets)) {
+        const targetSupported = getTargetValue(targets[targetKey], keyPath);
+
+        columns.push(
+          <td>
+            {isObject(nodeValue)
+              ? `${((targetTotals[targetKey] / baselineTotal) * 100).toFixed(
+                  0
+                )}%`
+              : targetSupported
+              ? supported
+              : unsupported}
+          </td>
+        );
       }
 
       rows.push(
@@ -94,13 +130,7 @@ function App() {
           <tr>
             <td>{key}</td>
             <td>{supported}</td>
-            <td>
-              {isObject(nodeValue)
-                ? `${((targetTotal / baselineTotal) * 100).toFixed(0)}%`
-                : targetSupported
-                ? supported
-                : unsupported}
-            </td>
+            {columns}
           </tr>
           {childRows}
         </>
@@ -110,21 +140,31 @@ function App() {
     return {
       rows,
       baselineTotal,
-      targetTotal,
+      targetTotals,
     };
   };
 
+  const targets = {
+    node18,
+    node20,
+    workerd,
+    wranglerJspm,
+    wranglerUnenv,
+    wranglerV3,
+  };
   return (
     <div className="App">
       <table>
         <thead>
           <tr>
             <th>API</th>
-            <th>Node 22</th>
-            <th>Node 18</th>
+            <th>{targetTitles["node22"]}</th>
+            {Object.keys(targets).map((target) => (
+              <th>{targetTitles[target as keyof typeof targetTitles]}</th>
+            ))}
           </tr>
         </thead>
-        {renderEntries(node22, []).rows}
+        {renderEntries(node22, targets, []).rows}
       </table>
     </div>
   );
